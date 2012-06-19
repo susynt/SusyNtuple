2dccbaaf45aba0765431d429f1fa27018c5c2bfe
Steven Farrel <sfarrell@cern.ch>
2012-06-19 21:49:55 +0000

    update makeTags.sh

--- scripts/makeTag.sh
+++ scripts/makeTag.sh
@@ -3,21 +3,26 @@
 # Use this script to easily create a SusyNtuple tag in svn
 # Need to pass the desired tag in as the only argument
 
-if [[ $# != 1 ]]; then
+if [[ $# < 2 ]]; then
 
     echo "Usage:"
-    echo "> makeTag.sh SusyNtuple-XX-YY-ZZ"
+    echo "> makeTag.sh SusyNtuple-XX-YY-ZZ \"My message\""
     exit 1
 
 fi
 
 tag=$1
+shift
+message=$@
 
 trunk=svn+ssh://svn.cern.ch/reps/atlasinst/Institutes/UCIrvine/SUSYAnalysis/SusyNtuple/trunk
 target=svn+ssh://svn.cern.ch/reps/atlasinst/Institutes/UCIrvine/SUSYAnalysis/SusyNtuple/tags/$tag
 
-echo "Copying trunk to"
-echo "  $target"
+
+echo "Copying trunk to:"
+echo "    $target"
+echo "With message:"
+echo "    $message"
 echo
 
 sleep 1s
@@ -34,6 +39,6 @@ fi
 
 echo "OK, tagging now..."
 
-svn cp $trunk $target
+svn cp $trunk $target -m "$message"
 
 echo "Done"
