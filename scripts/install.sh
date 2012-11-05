14da744a51a1a67d84b623154f87d1330a84b990
Steven Farrel <sfarrell@cern.ch>
2012-11-05 16:22:24 +0000

    update install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,7 +6,7 @@
 asetup AtlasProduction,17.3.1.1,setup,here
 
 # For now, we will need SUSYTools and Mt2 and DGTriggerReweighting
-svn co svn+ssh://svn.cern.ch/reps/atlasoff/PhysicsAnalysis/SUSYPhys/SUSYTools/tags/SUSYTools-00-01-07 SUSYTools
+svn co svn+ssh://svn.cern.ch/reps/atlasoff/PhysicsAnalysis/SUSYPhys/SUSYTools/tags/SUSYTools-00-01-10 SUSYTools
 svn co svn+ssh://svn.cern.ch/reps/atlasphys/Physics/SUSY/Analyses/WeakProduction/Mt2/tags/Mt2-00-00-01 Mt2
 svn co svn+ssh://svn.cern.ch/reps/atlasphys/Physics/SUSY/Analyses/WeakProduction/DGTriggerReweight/tags/DGTriggerReweight-00-00-15 DGTriggerReweight
 python SUSYTools/python/install.py
