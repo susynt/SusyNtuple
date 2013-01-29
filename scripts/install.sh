e106735c92fd16b8128a898b4fba8f33ac631a41
Alaettin Serhan Mete <alaettin.serhan.mete@cern.ch>
2013-01-29 12:53:40 +0000

    See ChangeLog

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,12 +3,12 @@
 # It is better to run kinit before this script
 
 # Run this from your work dir, not within this package
-asetup AtlasProduction,17.3.1.1,setup,here
+asetup AtlasProduction,17.3.4.6,setup,here
 
 # For now, we will need SUSYTools and Mt2 and DGTriggerReweighting
-svn co svn+ssh://svn.cern.ch/reps/atlasoff/PhysicsAnalysis/SUSYPhys/SUSYTools/tags/SUSYTools-00-02-02 SUSYTools
+svn co svn+ssh://svn.cern.ch/reps/atlasoff/PhysicsAnalysis/SUSYPhys/SUSYTools/tags/SUSYTools-00-02-03 SUSYTools
 svn co svn+ssh://svn.cern.ch/reps/atlasphys/Physics/SUSY/Analyses/WeakProduction/Mt2/tags/Mt2-00-00-01 Mt2
-svn co svn+ssh://svn.cern.ch/reps/atlasphys/Physics/SUSY/Analyses/WeakProduction/DGTriggerReweight/tags/DGTriggerReweight-00-00-23 DGTriggerReweight
+svn co svn+ssh://svn.cern.ch/reps/atlasphys/Physics/SUSY/Analyses/WeakProduction/DGTriggerReweight/tags/DGTriggerReweight-00-00-24 DGTriggerReweight
 python SUSYTools/python/install.py
 
 # Configure RootCore
@@ -19,5 +19,3 @@ cd ..
 
 # Compile everything
 $ROOTCOREDIR/scripts/build.sh
-
-
