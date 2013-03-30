a0fcb4ce37e0dfe9ecdf7e946287fcee3ff84203
Steven Farrel <sfarrell@cern.ch>
2013-03-30 21:17:41 +0000

    update lumi for A-L

--- SusyNtuple/SusyDefs.h
+++ SusyNtuple/SusyDefs.h
@@ -40,7 +40,8 @@ const float LUMI_A_B14 = 5835.;
 const float LUMI_A_D7  = 9632.86;
 const float LUMI_A_D   = 10454.;
 const float LUMI_A_E   = 13021.8;
-const float LUMI_A_L   = 20690.4;
+
+const float LUMI_A_L   = 20339.8; // 20690.4;
 
 //-----------------------------------------------------------------------------------
 // Convenience typedefs
