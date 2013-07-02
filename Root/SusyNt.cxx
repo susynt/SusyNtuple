ea8e27f5ee8b236513243b40e1a0da5e752be330
Matthew Relich <mrelich6@gmail.com>
2013-07-02 07:42:19 +0000

    Adding sumet to met terms

--- Root/SusyNt.cxx
+++ Root/SusyNt.cxx
@@ -529,24 +529,29 @@ Met::Met(const Met &rhs):
   refEle(rhs.refEle),
   refEle_etx(rhs.refEle_etx),
   refEle_ety(rhs.refEle_ety),
+  refEle_sumet(rhs.refEle_sumet),
   refMuo(rhs.refMuo),
   refMuo_etx(rhs.refMuo_etx),
   refMuo_ety(rhs.refMuo_ety),
+  refMuo_sumet(rhs.refMuo_sumet),
   refJet(rhs.refJet),
   refJet_etx(rhs.refJet_etx),
   refJet_ety(rhs.refJet_ety),
+  refJet_sumet(rhs.refJet_sumet),
   softJet(rhs.softJet),
   softJet_etx(rhs.softJet_etx),
   softJet_ety(rhs.softJet_ety),
   refGamma(rhs.refGamma),
   refGamma_etx(rhs.refGamma_etx),
   refGamma_ety(rhs.refGamma_ety),
+  refGamma_sumet(rhs.refGamma_sumet),
   refCell(rhs.refCell),
   refCell_etx(rhs.refCell_etx),
   refCell_ety(rhs.refCell_ety),
   softTerm(rhs.softTerm),
   softTerm_etx(rhs.softTerm_etx),
   softTerm_ety(rhs.softTerm_ety),
+  softTerm_sumet(rhs.softTerm_sumet),
   sys(rhs.sys)
 {
 }
@@ -562,21 +567,28 @@ Met& Met::operator=(const Met &rhs)
     refEle = rhs.refEle; 
     refEle_etx = rhs.refEle_etx; 
     refEle_ety = rhs.refEle_ety; 
+    refEle_sumet = rhs.refEle_sumet; 
     refMuo = rhs.refMuo; 
     refMuo_etx = rhs.refMuo_etx; 
     refMuo_ety = rhs.refMuo_ety; 
+    refMuo_sumet = rhs.refMuo_sumet; 
     refJet = rhs.refJet; 
     refJet_etx = rhs.refJet_etx; 
     refJet_ety = rhs.refJet_ety; 
+    refJet_sumet = rhs.refJet_sumet; 
     softJet = rhs.softJet; 
     softJet_etx = rhs.softJet_etx; 
     softJet_ety = rhs.softJet_ety; 
     refGamma = rhs.refGamma; 
     refGamma_etx = rhs.refGamma_etx; 
     refGamma_ety = rhs.refGamma_ety; 
+    refGamma_sumet = rhs.refGamma_sumet; 
     refCell = rhs.refCell; 
     refCell_etx = rhs.refCell_etx; 
     refCell_ety = rhs.refCell_ety; 
+    softTerm_etx = rhs.softTerm_etx;
+    softTerm_ety = rhs.softTerm_ety;
+    softTerm_sumet = rhs.softTerm_sumet;
     sys = rhs.sys; 
   }
   return *this;
--- SusyNtuple/SusyNt.h
+++ SusyNtuple/SusyNt.h
@@ -555,6 +555,7 @@ namespace Susy
       
       float Et;
       float phi;
+      float sumet;
 
       // Build a TLorentzVector on the fly
       const TLorentzVector lv() const {
@@ -567,24 +568,31 @@ namespace Susy
       float refEle;             // Ref electron term magnitude
       float refEle_etx;         // Ref electron term
       float refEle_ety;         // Ref electron term
+      float refEle_sumet;       // Ref electron sumet
       float refMuo;             // Ref muon term magnitude
       float refMuo_etx;         // Ref muon term
       float refMuo_ety;         // Ref muon term
+      float refMuo_sumet;       // Ref muon sumet
       float refJet;             // Ref jet term
       float refJet_etx;         // Ref jet term
       float refJet_ety;         // Ref jet term
+      float refJet_sumet;       // Ref jet sumet
       float softJet;            // Soft jet term
       float softJet_etx;        // Soft jet term
       float softJet_ety;        // Soft jet term
+      //float softJet_sumet;       // No Need, this term obsolete
       float refGamma;           // Ref gamma term
       float refGamma_etx;       // Ref gamma term
       float refGamma_ety;       // Ref gamma term
+      float refGamma_sumet;     // Ref gamma term
       float refCell;            // Cellout term
       float refCell_etx;        // Cellout term
       float refCell_ety;        // Cellout term
+      //float refCell_sumet;       // No Need, this term obsolete
       float softTerm;           // Soft term
       float softTerm_etx;       // Soft term
       float softTerm_ety;       // Soft term
+      float softTerm_sumet;       // Soft term sumet
 
       // I'm not a big fan of the way this is setup, would like to improve it someday
       int sys;                  // Met stored in vector for each sys shift, this identifies met for specific shift
@@ -594,14 +602,15 @@ namespace Susy
 
       // clear vars
       void clear(){
-        Et = phi = 0;
+        Et = phi = sumet = 0;
         refEle = refMuo = refJet = softJet = refGamma = refCell = softTerm = 0;
         refEle_etx = refMuo_etx = refJet_etx = softJet_etx = refGamma_etx = refCell_etx = softTerm_etx = 0;
         refEle_ety = refMuo_ety = refJet_ety = softJet_ety = refGamma_ety = refCell_ety = softTerm_ety = 0;
         sys = 0;
+	refEle_sumet = refMuo_sumet = refJet_sumet = refGamma_sumet = softTerm_sumet = 0;
       }
 
-      ClassDef(Met, 3);
+      ClassDef(Met, 4);
   };
 
   // TruthParticle
