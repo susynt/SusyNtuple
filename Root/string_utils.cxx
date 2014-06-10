bec18ec3e6d1afb00e42c2a5caa6823c5b94c36c
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2014-06-10 16:20:03 -0700

    string_utils: import a few functions from SusyXSReader
    These functions will be used to check the formatting of the xsec files
    before passing it on to CrossSectionDB

--- Root/string_utils.cxx
+++ Root/string_utils.cxx
@@ -1,10 +1,22 @@
 #include "SusyNtuple/string_utils.h"
 
+#include <algorithm> // transform
+#include <cstdlib> // strtod
+#include <functional> // multiplies
+#include <iostream>
+#include <numeric> // accumulate
+#include <iterator> // ostream_iterator, back_inserter
+#include <sstream> // istringstream
+#include <utility> // make_pair
+
+using std::vector;
 using std::string;
+using std::cout;
+using std::endl;
 
 using namespace susy::utils;
 
-//--------------------------------------
+//----------------------------------------------------------
 std::string susy::utils::rmLeadingTrailingWhitespaces(const std::string &str)
 {
   string result;
@@ -17,4 +29,36 @@ std::string susy::utils::rmLeadingTrailingWhitespaces(const std::string &str)
       result = str.substr(startpos, endpos-startpos+1);
   return result;
 }
-//--------------------------------------
+//----------------------------------------------------------
+bool BothAreSpaces(char lhs, char rhs) { return (lhs == rhs) && (lhs == ' '); }
+std::string susy::utils::multipleSpaces2singleSpace(std::string str)
+{ // from http://stackoverflow.com/questions/8362094/replace-multiple-spaces-with-one-space-in-a-string
+  std::string::iterator new_end = std::unique(str.begin(), str.end(), BothAreSpaces);
+ str.erase(new_end, str.end());
+ return str;
+}
+//----------------------------------------------------------
+bool susy::utils::contains(const std::string &str, const std::string &substr)
+{
+  return (str.find(substr)!=std::string::npos);
+}
+//----------------------------------------------------------
+std::vector< std::string > susy::utils::tokenizeString(const std::string &inputString, char separator)
+{
+  vector<string> tokens;
+  std::istringstream buffer(string(multipleSpaces2singleSpace(inputString)));
+  for(string token; getline(buffer, token, separator); /*nothing*/ ) tokens.push_back(token);
+  return tokens;
+}
+//----------------------------------------------------------
+double susy::utils::string2double(const std::string &s) { return strtod(s.c_str(), NULL); }
+//----------------------------------------------------------
+double susy::utils::multiply(const std::string &str)
+{
+  if(!contains(str, "*")) return string2double(str);
+  vector<string> tks(tokenizeString(str, '*'));
+  vector<double> factors;
+  std::transform(tks.begin(), tks.end(), std::back_inserter(factors), string2double);
+  return std::accumulate(factors.begin(), factors.end(), 1.0, std::multiplies<double>());
+}
+//----------------------------------------------------------
--- SusyNtuple/string_utils.h
+++ SusyNtuple/string_utils.h
@@ -19,7 +19,21 @@ namespace utils{
 
 typedef std::vector< std::string > vstring_t;
 
+/// return a copy of the str where the leading and trailing space+tab have been dropped
 std::string rmLeadingTrailingWhitespaces(const std::string &str);
+/// given str, return a copy where contiguous whitespaces have been replaced by single whitespaces
+/**
+   Do we need to worry about tab vs. space? DG-2014-06-10
+ */
+std::string multipleSpaces2singleSpace(std::string str);
+/// whether str contains substr
+bool contains(const std::string &str, const std::string &substr);
+/// split inputString in a vector of tokens
+std::vector< std::string > tokenizeString(const std::string &inputString, char separator);
+/// convert a string to a double
+double string2double(const std::string &s);
+/// convert a string to a double accounting for '*' multiplicative factors
+double multiply(const std::string &str);
 
 } // utils
 } // susy
