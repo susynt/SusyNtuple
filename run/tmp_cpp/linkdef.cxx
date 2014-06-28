6ca9887f38b131a25bc42adb0403c2b43cc22f66
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2014-06-28 03:12:47 -0700

    linkdef to generate dictionaries
    This linkdef contains the pragma declarations needed to generate the
    dictionaries allowing to access the collections from PyROOT.
    This file is in a separate directory to avoid polluting pwd
    with all the files generated automatically by aclic.

---
+++ run/tmp_cpp/linkdef.cxx
@@ -0,0 +1,23 @@
+#include "SusyNtuple/VarHandle.h"
+#include "SusyNtuple/SusyNt.h"
+#include <vector>
+#include <utility> // std::pair
+
+#ifdef __CINT__
+
+#pragma link C++ class pair<unsigned int,int>+;
+#pragma link C++ class D3PDReader::VarHandleBase+;
+#pragma link C++ class D3PDReader::VarHandle<vector<Susy::TruthParticle>*>+;
+#pragma link C++ class D3PDReader::VarHandle<vector<Susy::TruthJet>*>+;
+#pragma link C++ class D3PDReader::VarHandle<vector<Susy::TruthMet>*>+;
+#pragma link C++ class D3PDReader::VarHandle<Susy::Event*>+;
+#pragma link C++ class D3PDReader::VarHandle<vector<Susy::Electron>*>+;
+#pragma link C++ class D3PDReader::VarHandle<vector<Susy::Muon>*>+;
+#pragma link C++ class D3PDReader::VarHandle<vector<Susy::Jet>*>+;
+#pragma link C++ class D3PDReader::VarHandle<vector<Susy::Photon>*>+;
+#pragma link C++ class D3PDReader::VarHandle<vector<Susy::Tau>*>+;
+#pragma link C++ class D3PDReader::VarHandle<vector<Susy::Met>*>+;
+
+#pragma link C++ class std::vector<Susy::Lepton*>+;
+
+#endif
