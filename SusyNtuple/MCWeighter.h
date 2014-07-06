bd280af0aae5710329034ec46952f47f62c85ce7
gerbaudo <davide.gerbaudo@gmail.com>
2014-07-06 11:39:11 +0200

    leftover formatting, [build-doxygen]

--- SusyNtuple/MCWeighter.h
+++ SusyNtuple/MCWeighter.h
@@ -8,13 +8,10 @@
 #include "SusyNtuple/SusyNt.h"
 
 #include <string>
-/*
-
-    MCWeighter - a class to handle the normalization of Monte Carlo
-
+/// A class to handle the normalization of Monte Carlo
+/**
     There are options to control how to retrieve the sumw, xsec, etc.
     The defaults are recommended, as long as they work.
-
 */
 
 class MCWeighter
@@ -61,35 +58,37 @@ class MCWeighter
                std::string xsecDir="$ROOTCOREDIR/data/SUSYTools/mc12_8TeV/");
     ~MCWeighter();
 
-    // Build a map of MCID -> sumw.
-    // This method will loop over the input files associated with the TChain. The MCID
-    // in the first entry of the tree will be used, so one CANNOT use this if multiple
-    // datasets are combined into one SusyNt tree file! The generator weighted cutflow
-    // histograms will then be used to calculate the total sumw for each MCID. Each
-    // dataset used here must be complete, they CANNOT be spread out across multiple jobs.
-    // However, one can have more than one (complete) dataset in the chain, which is why
-    // we use the map.
+    /// Build a map of MCID -> sumw.
+    /**
+     This method will loop over the input files associated with the TChain. The MCID
+     in the first entry of the tree will be used, so one CANNOT use this if multiple
+     datasets are combined into one SusyNt tree file! The generator weighted cutflow
+     histograms will then be used to calculate the total sumw for each MCID. Each
+     dataset used here must be complete, they CANNOT be spread out across multiple jobs.
+     However, one can have more than one (complete) dataset in the chain, which is why
+     we use the map.
+    */
     void buildSumwMap(TTree* tree);
     void clearAndRebuildSumwMap(TTree* tree) { m_sumwMap.clear(); buildSumwMap(tree); }
     void dumpSumwMap();
 
-    // Specify methods to retrieve sumw and xsec
+    /// Specify methods to retrieve sumw and xsec
     void setUseProcSumw(bool useProcSumw=true) { m_useProcSumw = useProcSumw; }
     void setSumwMethod(SumwMethod opt=Sumw_MAP) { m_sumwMethod = opt; }
     void setXsecMethod(XsecMethod opt=Xsec_ST) { m_xsecMethod = opt; }
 
-    // MC Weight includes generator, xsec, lumi, and pileup weights
-    // Default weight uses 2012 A-D lumi. You can supply a different luminosity,
-    // but the pileup weights will still correspond to A-D.
+    /// MC Weight includes generator, xsec, lumi, and pileup weights
+    /** Default weight uses 2012 A-D lumi. You can supply a different luminosity,
+        but the pileup weights will still correspond to A-D. */
     float getMCWeight(const Susy::Event* evt, float lumi = LUMI_A_L, WeightSys sys=Sys_NOM);
     bool sumwmapHasKey(SumwMapKey k);
 
-    // Get sumw for this event
+    /// Get sumw for this event
     float getSumw(const Susy::Event* evt);
-    // Get cross section for this event
+    /// Get cross section for this event
     SUSY::CrossSectionDB::Process getCrossSection(const Susy::Event* evt);
     float getXsecTimesEff(const Susy::Event* evt, WeightSys sys=Sys_NOM);
-    // Get the pileup weight
+    /// Get the pileup weight
     float getPileupWeight(const Susy::Event* evt, WeightSys sys=Sys_NOM);
     /// specify the bin used to compute sumw; requires you to call clearAndRebuildSumwMap.
     /**
@@ -138,7 +137,7 @@ class MCWeighter
     // Map of (MCID, proc) -> sumw
     SumwMap m_sumwMap;
 
-    // SUSYTools cross sections
+    /// SUSYTools cross sections
     SUSY::CrossSectionDB m_xsecDB;
     XSecMap m_xsecCache;
 
--- SusyNtuple/SusyNtTruthAna.h
+++ SusyNtuple/SusyNtTruthAna.h
@@ -11,6 +11,7 @@
 #include <map>
 #include <set>
 
+/// Example SusyNt-based truth study
 class SusyNtTruthAna : public TSelector
 {
 
