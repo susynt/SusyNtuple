3caa3e0ee7dac46409da912bdcffa14cb1be3f45
dantrim <dantrim@uci.edu>
2016-02-22 11:59:06 +0100

    update PhotonSelector
    - Now of same form as other selectors

--- Root/PhotonSelector.cxx
+++ Root/PhotonSelector.cxx
@@ -1,108 +1,98 @@
 #include "SusyNtuple/PhotonSelector.h"
-#include "SusyNtuple/SusyNt.h"
+#include "SusyNtuple/Photon.h"
 
-using Susy::PhotonSelector;
-using Susy::Photon;
-using Susy::NtSys::SusyNtSys;
+#include <iostream>
+
 using std::cout;
 using std::endl;
 using std::string;
 
 namespace Susy {
+// -------------------------------------------------------------------------------------------- //
+PhotonSelector* PhotonSelector::build(const AnalysisType &a, bool verbose)
+{
+    PhotonSelector *p;
+    switch(a) {
+    case AnalysisType::Ana_2Lep :
+        p = new PhotonSelector_2Lep();
+        p->setSignalIsolation(Isolation::FixedCutTight);
+        break;
+    case AnalysisType::Ana_3Lep :
+        p = new PhotonSelector_3Lep();
+        p->setSignalIsolation(Isolation::FixedCutTight);
+        break;
+    case AnalysisType::Ana_2LepWH :
+        p = new PhotonSelector_2LepWH();
+        p->setSignalIsolation(Isolation::FixedCutTight);
+        break;
+    case AnalysisType::Ana_SS3L :
+        p = new PhotonSelector_SS3L();
+        p->setSignalIsolation(Isolation::FixedCutTight);
+        break;
+    case AnalysisType::Ana_Stop2L :
+        p = new PhotonSelector_Stop2L();
+        p->setSignalIsolation(Isolation::FixedCutTight);
+        break;
+    default :
+        cout << "PhotonSelector::build(): unknown analysis type '" << AnalysisType2str(a) << "'"
+             << " returning vanilla PhotonSelector" << endl;
+        p = new PhotonSelector();
+    } // switch
+    return p;
+}
+
+
 
 // -------------------------------------------------------------------------------------------- //
 // Constructor
 // -------------------------------------------------------------------------------------------- //
-PhotonSelector::PhotonSelector() :
-    //////////////////////////
-    // default selection
-    //////////////////////////
-
-    //////////////////////////
-    m_systematic(NtSys::NOM),
-    m_analysis(AnalysisType::kUnknown),
-    m_sigIso(Isolation::IsolationInvalid),
-    m_2lep(false),
-    m_3lep(false),
-    m_2lepWH(false),
-    m_SS3L(false),
+PhotonSelector::PhotonSelector():
+    m_signalIsolation(Isolation::IsolationInvalid),
     m_verbose(false)
 {
 }
+
 // -------------------------------------------------------------------------------------------- //
-PhotonSelector& PhotonSelector::setAnalysis(const AnalysisType &a)
+bool PhotonSelector::isBaseline(const Photon* ph)
 {
-    
-    //////////////////////////////////////
-    // Set analysis-specific cuts
-    //////////////////////////////////////
-
-    //////////////////////////////////////
-    // 2L-Analysis
-    //////////////////////////////////////
-    if( a == AnalysisType::Ana_2Lep ) {
-        m_2lep = true;
-        m_sigIso = Isolation::FixedCutTight;
+    bool pass = false;
+    if(ph) {
+        bool pass_author = (ph->authorPhoton || ph->authorAmbiguous); // reco. as photon OR (photon AND ele)
+        pass = (ph->tight &&
+                ph->Pt()        > 25 &&
+                std::abs(ph->clusEta) < 2.37 &&
+                pass_author);
     }
-    //////////////////////////////////////
-    // 3L-Analysis
-    //////////////////////////////////////
-    else if ( a == AnalysisType::Ana_3Lep ) {
-        m_3lep = true;
-        m_sigIso = Isolation::FixedCutTight;
-    }
-    //////////////////////////////////////
-    // WH-Analysis
-    //////////////////////////////////////
-    else if ( a == AnalysisType::Ana_2LepWH ) {
-        m_2lepWH = true;
-        m_sigIso = Isolation::FixedCutTight;
-    }
-    //////////////////////////////////////
-    // SS3L-Analysis
-    //////////////////////////////////////
-    else if ( a == AnalysisType::Ana_SS3L ) {
-        m_SS3L = true;
-        m_sigIso = Isolation::FixedCutTight;
-    }
-    //////////////////////////////////////
-    // Didn't set AnalysisType
-    //////////////////////////////////////
-    else if( a == AnalysisType::kUnknown ) {
-        string error = "PhotonSelector::setAnalysis error: ";
-        cout << error << "The AnalysisType has not been set for PhotonSelector. Check that you properly call" << endl;
-        cout << error << "'setAnalysis' for PhotonSelector for your analysis." << endl;
-        cout << error << ">>> Exiting." << endl;
-        exit(1);
-    }
-    //////////////////////////////////////
-    // Gone fishin'
-    //////////////////////////////////////
-    else {
-        string error = "PhotonSelector::setAnalysis error: ";
-        cout << error << "PhotonSelector is not configured for the AnalysisType (" << AnalysisType2str(a) << ")  provided in " << endl;
-        cout << error << "'setAnalysis'! Be sure that your AnalysisType is provided for in SusyNtuple/AnalysisType.h" << endl;
-        cout << error << "and that you provide the necessary requirements for your analysis in 'setAnalysis'." << endl;
-        cout << error << ">>> Exiting." << endl;
-        exit(1);
-    }
-
-    // check that the isolation has been set
-    if( m_sigIso == Isolation::IsolationInvalid ) {
-        string error = "PhotonSelector::setAnalysis error: ";
-        cout << error << "You have not configured the isolation quality for your photon selection." << endl;
-        cout << error << ">>> Exiting." << endl;
-        exit(1);
-    }
-    
-    m_analysis = a;
-
-    if(m_verbose)
-        cout << "PhotonSelector    Configured photon selection for AnalysisType " << AnalysisType2str(m_analysis) << endl;
-
-    return *this;
+    return pass;
 }
+// -------------------------------------------------------------------------------------------- //
+bool PhotonSelector::isSignal(const Photon* ph)
+{
+    bool pass = false;
+    if(ph) {
+        pass = (isBaseline(ph) &&
+                ph->isoFixedCutTight);
+    }
+    return pass;
+}
+// -------------------------------------------------------------------------------------------- //
+// begin PhotonSelector_2Lep Ana_2Lep
+// -------------------------------------------------------------------------------------------- //
 
 // -------------------------------------------------------------------------------------------- //
-//
+// begin PhotonSelector_3Lep Ana_3Lep
+// -------------------------------------------------------------------------------------------- //
+
+// -------------------------------------------------------------------------------------------- //
+// begin PhotonSelector_2LepWH Ana_2LepWH
+// -------------------------------------------------------------------------------------------- //
+
+// -------------------------------------------------------------------------------------------- //
+// begin PhotonSelector_SS3L Ana_SS3L
+// -------------------------------------------------------------------------------------------- //
+
+// -------------------------------------------------------------------------------------------- //
+// begin PhotonSelector_Stop2L Ana_Stop2L
+// -------------------------------------------------------------------------------------------- //
+
 } // namespace Susy
--- SusyNtuple/PhotonSelector.h
+++ SusyNtuple/PhotonSelector.h
@@ -2,8 +2,6 @@
 #ifndef SUSYNTUPLE_PHOTONSELECTOR_H
 #define SUSYNTUPLE_PHOTONSELECTOR_H
 
-#include "SusyNtuple/SusyDefs.h"
-#include "SusyNtuple/SusyNtSys.h"
 #include "SusyNtuple/AnalysisType.h"
 #include "SusyNtuple/Isolation.h"
 
@@ -12,33 +10,80 @@ namespace Susy {
     class Photon;
 
     /// A class to select photons
+    /**
+        The generic PhotonSelector implements the generic definitions from
+        https://twiki.cern.ch/twiki/bin/viewauth/AtlasProtected/SusyObjectDefinitionsr2013TeV#Photons
+
+        Analysis-dependent criteria should be implemented in your
+        analysis-specific class inheriting from PhotonSelector.
+
+        The analysis-specific selector should be instantiated with PhotonSelector::build().
+    */ 
+
     class PhotonSelector
     {
         public :
-        PhotonSelector();
-        PhotonSelector& setSystematic(const NtSys::SusyNtSys& systematic);
-        PhotonSelector& setAnalysis(const AnalysisType& analysis);
+        /// provide analysis-specific selector (or vanila one of analysis is unknown)
+        /**
+            The user owns the selector (i.e. should use std::shared_ptr or delete it
+            when doen with it).
+        */
+        static PhotonSelector* build(const AnalysisType &a, bool verbose);
+        PhotonSelector(); ///< Default ctor
+        virtual ~PhotonSelector() {}; ///< dtor (for now we don't have anything to delete)
+        virtual bool isBaseline(const Photon* ph); ///< whether photon passes the baseline criteria
+        /// whether photon passes the signal criteria
+        virtual bool isSignal(const Photon* ph);
 
+        Isolation signalIsolation() const { return m_signalIsolation; }
+
+        /// set signal isolation
+        /**
+            Note: the value you set here should match whatever you have in _your_
+            (overriding) implementation of isSignal()
+        */
+        PhotonSelector& setSignalIsolation(const Isolation &iso) { m_signalIsolation = iso; return *this; }
+
+        PhotonSelector& setVerbose(const bool &v) { m_verbose = v; return *this; }
+        bool verbose() const { return m_verbose; }
 
         protected :
-        NtSys::SusyNtSys m_systematic;
-        AnalysisType m_analysis;
 
-        Isolation m_sigIso;                 ///< photon isolation quality for photons
-
-        ///////////////////////////////
-        // Available analyses
-        ///////////////////////////////
-        bool m_2lep;    ///< flag for 2L analysis
-        bool m_3lep;    ///< flag for 3L analysis
-        bool m_2lepWH;  ///< flag for 2L WH analysis
-        bool m_SS3L;    ///< flag for the strong SS3L analysis 
-
-        // set verbose
+        Isolation m_signalIsolation;
         bool m_verbose;
-    
-    }; // class
+    }; // class PhotonSelector
 
+
+// ------------------------------------------------------------------
+//
+// End generic selector, begin analysis-specific ones
+//
+// ------------------------------------------------------------------
+
+    /// Implement photon selector for Ana_2Lep
+    class PhotonSelector_2Lep : public PhotonSelector
+    {
+    };
+
+    /// Implement photon selector for Ana_3Lep
+    class PhotonSelector_3Lep : public PhotonSelector
+    {
+    };
+
+    /// Implement photon selector for Ana_2LepWH
+    class PhotonSelector_2LepWH : public PhotonSelector
+    {
+    };
+
+    /// Implement photon selector for Ana_SS3L
+    class PhotonSelector_SS3L : public PhotonSelector
+    {
+    };
+
+    /// Implement photon selector for Ana_Stop2L
+    class PhotonSelector_Stop2L : public PhotonSelector
+    {
+    };
 
 } // namespace Susy
 
